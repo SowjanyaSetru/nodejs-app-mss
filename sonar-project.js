89bccb0edf3e63be53aeecbfc2d3f34bdaeddc4c
SowjanyaSetru <sowjanyasetru95@gmail.com>
2024-10-03 19:24:51 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://13.211.191.21:9000/',
+  serverUrl: 'http://3.27.63.95:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
